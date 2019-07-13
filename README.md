3a3d1f095378d5353a7fe936e638cf37d7033127
Jumpy-Squirrel <jsquirrel_github_9a6d@packetloss.de>
2019-07-13 23:46:32 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,80 @@
+# UI Tests for the Eurofurence Hotelbooking App
+
+Implemented using [testcafe](https://github.com/DevExpress/testcafe) in JavaScript (ECMAScript6 to be precise).
+
+## Test Cases To Be Automated
+
+### Title Page
+
+ - T1: When accessing the main URL, the title page is shown in English
+ - T2: On the title page, switching language to German works (check text/button captions etc.)
+ - T3: On the title page, switching language back to English works (check text/button captions etc.)
+ - T4: On the title page, continuing to the form page works
+
+### Form Page
+
+ - F1: On the form page, initial setup when first switching there is sensible 
+   (single room, first room type, default arrival and departure date, disclaimer not ticked, fields enabled)
+ - F2: On the form page, the user can enter booking information for a single room and the correct prices are shown
+ - F3: On the form page, the user can enter booking information for a double room and the correct prices are shown
+ - F4: On the form page, the user can enter booking information for a triple room and the correct prices are shown
+ - F5: On the form page, the user can enter a long message in the comments field (try 5000 characters)
+ - F6: On the form page, before the disclaimer checkbox is checked, the "Generate Email" button is disabled, 
+   and clicking it produces an error message and does not proceed to the email page
+ - F7: On the form page, switching language from English to German works 
+   (field values and captions show correctly, and all entered information is retained, including date with format change)
+ - F8: On the form page, switching language from German to English works
+   (field values and captions show correctly, and all entered information is retained, including date with format change)
+ - F9: On the form page, after the disclaimer checkbox is checked, the submit button becomes available and it works
+ - F10 TODO: On the form page, these fields are mandatory for the first person:
+   Name (min 3 characters), Street Address (min 3 characters), City and ZIP (min 3 characters), 
+   Country (min 3 characters), Email address (no spaces, must contain an @), Phone Number (min 3 characters).
+   If any of them is not set initially or cleared, the Generate Email button stays/becomes unavailable and the fields 
+   are somehow marked as "this field causes an error".
+ - F11 TODO: On the form page, even when double or triple room is selected, their personal information can be left
+   blank as long as the info for the first person is provided. An email can be generated that states that
+   data for the second or third person is not provided (to avoid booking errors due to misreading by the hotel)
+
+### Email Page
+
+ - E1: On the email page, before the secret code has been made available, the proper secret phrase, 
+   convention keyword and target email address are not shown, and this is very clearly indicated in the generated mail text.
+   No link for sending the mail message with external mail programs is generated.
+ - E2: On the email page, after the secret code has been made available, the proper secret phrase,
+   convention keyword and target email address ARE shown, the fact that you can now send the mail is made clear,
+   and a link for sending the mail message with an external mail program is generated.
+ - E3: On the email page, the captions and mail text are correct for German - single room - first room option
+   (and the room type is spelled out, not given as a number)
+   (and exactly one set of personal info is printed in the email )
+ - E4: On the email page, the captions and mail text are correct for German - double room - second room option
+   leave the info for the second person blank in this case
+   (and the room type is spelled out, not given as a number)
+   (and exactly two sets of personal info are printed in the email, with the second being shown as not provided)
+ - E5: On the email page, the captions and mail text are correct for German - triple room - last room option
+   (and the room type is spelled out, not given as a number)
+   (and exactly three sets of personal info are printed in the email)
+ - E6: On the email page, the captions and mail text are correct for English - single room - first room option
+   (and the room type is spelled out, not given as a number)
+   (and exactly one set of personal info is printed in the email )
+ - E7: On the email page, the captions and mail text are correct for English - double room - second room option
+   (and the room type is spelled out, not given as a number)
+   (and exactly two sets of personal info are printed in the email)
+ - E8: On the email page, the captions and mail text are correct for English - triple room - last room option
+   leave the personal info for the second and third person blank in this case
+   (and the room type is spelled out, not given as a number)
+   (and exactly three sets of personal info are printed in the email, with the second and third being shown as
+   not provided)
+ - E9: On the email page, if a long message was entered in the comments field, it is included as is in the mail
+ - E10: On the email page, switching languages from German to English works, and the mail properly changes language
+   (including field content formatting, use case E3 above)
+ - E11: On the email page, switching languages from English to German works, and the mail properly changes language 
+   (including field content formatting, use case E8 above to test a different combination)
+ - E12: On the email page, the content of all input fields on the email page cannot be edited
+ - E13 TODO: On the email page, there is an easy to understand way to get back to the form page 
+ - E14 TODO: On the email page, the page states how long until the secret code will be made available, and then
+   it automatically reloads with all information intact
+     
+## Manual Test Cases / Code Verification
+
+ - Check that it uses server side script response for time to activation of secret code
+ - Try the app on various mobile devices and with various desktop browsers
---
+++ page-model.js
@@ -0,0 +1,182 @@
+import {Selector, ClientFunction} from 'testcafe';
+
+export class TitlePage {
+    constructor(t) {
+        this.t = t;
+        this.language = 'en';
+
+        this.getLocation = ClientFunction(() => document.location.href);
+
+        // Our Buttons and their labels do not have an id yet - TODO give them one
+        this.navs = {
+            submitButton: Selector('a[href="reservation-form.html"]'),
+            languageDropdown: Selector('a[class="dropdown-toggle"]'),
+            englishButton: Selector('a[href="../en/index.html"]'),
+            germanButton: Selector('a[href="../de/index.html"]'),
+        };
+        // Most of our text elements do not have ids yet - TODO give them one
+        this.labels = {
+            textHeadline: Selector('#unhide_with_js div h4'),
+        };
+    }
+
+    async switchToGerman() {
+        this.language = 'de';
+        await this.t
+            .click(this.navs.languageDropdown)
+            .click(this.navs.germanButton);
+    }
+
+    async switchToEnglish() {
+        this.language = 'en';
+        await this.t
+            .click(this.navs.languageDropdown)
+            .click(this.navs.englishButton);
+    }
+
+    async checkLanguage() {
+        // TODO once the full text has an id, we can compare the whole thing
+        var expectedHeadline = this.language === 'en' ? 'Booking a Hotelroom for Eurofurence' : 'Hotelbuchungsprozedur für Eurofurence';
+
+        await this.t
+            .expect(this.getLocation()).contains('/' + this.language + '/')
+            // automatically selects the first match I think, get second match using .nth(1)
+            .expect(this.labels.textHeadline.innerText).contains(expectedHeadline);
+    }
+
+    async submit() {
+        await this.t
+            .click(this.navs.submitButton)
+            .expect(this.getLocation()).contains('/' + this.language + '/reservation-form.html');
+    }
+
+    toFormPage() {
+        return new FormPage(this.t, this.language);
+    }
+}
+
+export class FormPage {
+    constructor(t, language) {
+        this.t = t;
+        this.language = language;
+
+        this.getLocation = ClientFunction(() => document.location.href);
+
+        this.navs = {
+            englishButton: Selector('a[href="../en/reservation-form.html"]'),
+            germanButton: Selector('a[href="../de/reservation-form.html"]'),
+            submitButton: Selector('#submitbutton'),
+        };
+        this.labels = {
+            roomsize: [Selector('#size_single_label'), Selector('#size_double_label'), Selector('#size_triple_label')],
+            // it makes no sense to have the labels for column 2 now that column 1 is editable
+            // TODO change this in the app and here
+            name: Selector('label[for="name2"]'),
+            street: Selector('label[for="street2"]'),
+            city: Selector('label[for="city2"]'),
+            country: Selector('label[for="country2"]'),
+            email: Selector('label[for="email2"]'),
+            phone: Selector('label[for="phone2"]'),
+            dates: Selector('label[for="arrival"]'),
+            // TODO this label really needs an id
+            // roomtype: Selector('label[for="arrival"]'),
+            roomtypes: [Selector('#roomtype1button'), Selector('#roomtype2button'), Selector('#roomtype3button')],
+            price: [Selector('#roomtype1price'), Selector('#roomtype2price'), Selector('#roomtype3price')],
+        };
+        this.fields = {
+            name: [Selector('#name1'), Selector('#name2'), Selector('#name3')],
+            street: [Selector('#street1'), Selector('#street2'), Selector('#street3')],
+            city: [Selector('#city1'), Selector('#city2'), Selector('#city3')],
+            country: [Selector('#country1'), Selector('#country2'), Selector('#country3')],
+            email: [Selector('#email1'), Selector('#email2'), Selector('#email3')],
+            phone: [Selector('#phone1'), Selector('#phone2'), Selector('#phone3')],
+            arrival: Selector('#arrival'),
+            departure: Selector('#departure'),
+            roomtype: [Selector('#roomtype1button'), Selector('#roomtype2button'), Selector('#roomtype3button')],
+            disclaimer: Selector('#understood'),
+        };
+    }
+
+    async setRoomsize(sizeNo) {
+        await this.t
+            .click(this.labels.roomsize[sizeNo - 1]);
+    }
+
+    async _verifyInputDisabledState(selector, isDisabled) {
+        if (isDisabled) {
+            await this.t.expect(selector.hasAttribute('disabled')).ok();
+        } else {
+            await this.t.expect(selector.hasAttribute('disabled')).notOk();
+        }
+    }
+
+    async verifyRoomsizeAndFieldsEnabled(sizeNo) {
+        var shouldBeDisabled = [false, (sizeNo < 2), (sizeNo < 3)];
+        for (var i = 0; i < 3; i++) {
+            await this._verifyInputDisabledState(this.fields.name[i], shouldBeDisabled[i]);
+            await this._verifyInputDisabledState(this.fields.street[i], shouldBeDisabled[i]);
+            await this._verifyInputDisabledState(this.fields.city[i], shouldBeDisabled[i]);
+            await this._verifyInputDisabledState(this.fields.country[i], shouldBeDisabled[i]);
+            await this._verifyInputDisabledState(this.fields.email[i], shouldBeDisabled[i]);
+            await this._verifyInputDisabledState(this.fields.phone[i], shouldBeDisabled[i]);
+        }
+    }
+
+    async verifyInitialState() {
+        await this.t
+            .expect(this.fields.arrival.value).eql('08/14/2019')
+            .expect(this.fields.departure.value).eql('08/18/2019')
+            // TODO how to get the value of a radio button????
+            // .expect(this.fields.roomtype[0].selected).eql(true)
+            .expect(this.fields.disclaimer.checked).notOk()
+    }
+
+    async setName(personNo, value) {
+        await this.t
+            .typeText(this.fields.name[personNo - 1], value);
+    }
+
+    async setStreet(personNo, value) {
+        await this.t
+            .typeText(this.fields.street[personNo - 1], value);
+    }
+
+    async setCity(personNo, value) {
+        await this.t
+            .typeText(this.fields.city[personNo - 1], value);
+    }
+
+    async setCountry(personNo, value) {
+        await this.t
+            .typeText(this.fields.country[personNo - 1], value);
+    }
+
+    async setEmail(personNo, value) {
+        await this.t
+            .typeText(this.fields.email[personNo - 1], value);
+    }
+
+    async setPhone(personNo, value) {
+        await this.t
+            .typeText(this.fields.phone[personNo - 1], value);
+    }
+
+    async verifyPrice(roomtypeNo, valueString) {
+        await this.t
+            .expect(this.labels.price[roomtypeNo - 1].innerText).eql(valueString + " €*");
+    }
+}
+
+export class EmailPage {
+    constructor(t, language) {
+        this.t = t;
+        this.language = language;
+
+        this.navs = {
+            englishButton: Selector('a[href="../en/reservation-show.html"]'),
+            germanButton: Selector('a[href="../de/reservation-show.html"]'),
+        };
+    }
+}
+
+
